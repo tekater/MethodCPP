df178d0b43ba11621b031201acf2072defc079d1
tekater <120014627+tekater@users.noreply.github.com>
2023-04-24 20:35:48 +0900

    Create _work_file2.h

---
+++ _work_file2.h
@@ -0,0 +1,11 @@
+#pragma warning(disable:4996) // Очень важно - без этого strcat не работает.
+
+#include <iostream>
+#include <stdlib.h>  // rand(), srand()
+#include <windows.h> // time()
+#include <math.h>    // библиотека матеши
+#include <time.h>
+
+using namespace std;
+
+
